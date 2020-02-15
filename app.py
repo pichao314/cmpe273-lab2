b67382d23a0f4fcf7f16f09efe04163166f25c26
pcmbp <pichao314@gmail.com>
2020-02-14 20:01:15 -0800

    add student id check

--- app.py
+++ app.py
@@ -2,23 +2,51 @@ from flask import Flask, escape, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
-student = [
-    {
-        'id': 0,
-        'name': "Chao Pi",
-    }
-]
+
+students = [{
+    'id': 0,
+    'name': "Chao Pi",
+}]
+
 
 @app.route('/students')
 def sums():
-    p = "" if len(student) <= 1 else "s"
-    return "Totally %d student" % len(student) + p,200
+    p = "" if len(students) <= 1 else "s"
+    return "Totally %d student" % len(students) + p, 200
+
 
 @app.route('/students/<int:id>')
 def get(id):
-    return jsonify(student[id]),200
+    for student in students:
+        if student['id']==id:
+            return jsonify(student), 200
+    return "Not Found!",202
+
+
+@app.route('/students', methods=['POST'])
+def post():
+    if not request.json:
+        return "Wrong Value"
+    info = request.json
+    for student in students:
+        if info['id']==student['id']:
+            return "ID existed!", 202
+    students.append({'id': info['id'], 'name': info['name']})
+    return "Added!", 201
+
+classes = [
+    {
+        "id":273,
+        "name":"CMPE-273",
+        "students":[]
+    }
+]
+
+
+
