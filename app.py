84db6dd4892753c75c8995b9169bd14755f25253
pcmbp <pichao314@gmail.com>
2020-02-14 19:40:02 -0800

    GET student tested

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,3 +6,19 @@ app = Flask(__name__)
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+student = [
+    {
+        'id': 0,
+        'name': "Chao Pi",
+    }
+]
+
+@app.route('/students')
+def sums():
+    p = "" if len(student) <= 1 else "s"
+    return "Totally %d student" % len(student) + p,200
+
+@app.route('/students/<int:id>')
+def get(id):
+    return jsonify(student[id]),200
